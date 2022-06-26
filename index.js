1ae0d1b9da0796f0bca2c543ed6d5650d72e937f
Tergel <tergel7006@gmail.com>
2022-06-27 01:10:26 +0800

    the logic for changing the text of a div

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 axios.get('http://localhost:3000/words').then(resp => {
   console.log(resp.data);
+  var data = resp.data;
+  for (let key in data) {
+    if (data.hasOwnProperty(key))
+    {
+        value = data[key];
+        console.log(key, value);
+    }
+  }
 });
 
 const cards = document.querySelectorAll('.card');
@@ -24,51 +32,7 @@ function flip() {
 
 cards.forEach(card => card.addEventListener('click', flip))
 
-
-
-
-// Attempt to get the words form a json file and give it to the data property of the cards
-
-// const fs = require('fs');
-
-// var rawData = fs.readFileSync('data.json');
-// var words_obj = JSON.parse(rawData);
-// var words = []
-
-// function sortData(words_obj, words) {
-//   for (word in words_obj) {
-//     words.push(word)
-//     words.push(words_obj[word])
-//   }
-// }
-// sortData(words_obj, words)
-
-// function shuffle(arr) {
-//   let currentIndex = arr.length,  randomIndex;
-
-//   while (currentIndex != 0) {
-
-//     randomIndex = Math.floor(Math.random() * currentIndex);
-//     currentIndex--;
-
-//     [arr[currentIndex], arr[randomIndex]] = [
-//       arr[randomIndex], arr[currentIndex]];
-//   }
-
-//   return arr;
-// }
-
-// var words_shuffled = shuffle(words)
-
-// function giveData (data) {
-//   for (var x=0; x<data.length; x++) {
-//     for (var i=0; i<=16; i++) {
-//       var div = document.getElementById(i.toString())
-//       div.dataset.word = data[x]
-//       console.log('asfsfsf')
-//       break
-//     }
-//   }
-// }
-
-// giveData(words_shuffled)
+var front = document.getElementById('1').getElementsByClassName('front')[0]
+var back = document.getElementById('1').getElementsByClassName('back')[0]
+front.textContent = 'sdfghjk'
+back.textContent = '123'
